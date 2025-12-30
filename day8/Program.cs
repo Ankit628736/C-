5879dbff4e838915d1302baf59466f9fb663e7fc
ankit628736 <singh.ankit9871@gmail.com>
2025-12-30 11:13:36 +0530

    second commit

--- day8/Program.cs
+++ day8/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string fname,lname;
+        fname = "Rowan";
+        lname = "Akkinson";
+
+        char[] letter = {'H','e','l','l','o'};
+        char[] salary = {"Hello","From","Points"};
+
+        string fullname = fname+lname;
+        Console.WriteLine("full name {0}",fullname);
+
+        string greeting = new string(letter);
+        Console.WriteLine("Greeting: {0}",greeting);
+
+        string sarray = "hello";
+        string message = string.Join(" ",sarray);
+        Console.WriteLine("Message {0}",message);
+
+        DateTime wating = new DateTime(2012,10,10,1017,58,1);
+        
+    }
+}
